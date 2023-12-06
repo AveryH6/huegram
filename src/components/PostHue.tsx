0a4ab5f9695a1e13bde17cbee6e6f97f4faed93e
AveryH6 <89232897+AveryH6@users.noreply.github.com>
2023-12-06 12:30:33 -0600

    length change

--- src/components/PostHue.tsx
+++ src/components/PostHue.tsx
@@ -13,7 +13,7 @@ const PostHue = (props:Props) => {
     <div className='flex flex-col w-fit h-fit p-8 justify-evenly gap-8 border-2 border-gray-700 rounded-3xl'>
 
         <div className='flex flex-col w-full p-4 gap-4' style={{backgroundColor: color}}>
-            <input type="text" name="hue" id="hue" defaultValue={"#"} className='p-2 border-2 border-gray-700 rounded-3xl' onChange={ (event) => setColor(event.target.value) } />
+            <input type="text" name="hue" id="hue" defaultValue={"#"} maxLength={7} className='p-2 border-2 border-gray-700 rounded-3xl' onChange={ (event) => setColor(event.target.value) } />
             <a href="#" onClick={ () => props.addHue(color)  } className="btn bg-white border-2 border-gray-700 rounded-2xl text-gray-700 text-center">Post</a>
         </div>
 
